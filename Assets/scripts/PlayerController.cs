9e3638e498625887b19ce9e13a1ba80ff7e37b08
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-24 23:34:43 +0000

    Add VSCode plugin, and give main character a controller

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+    private float moveSpeed = 10f;
+    private CharacterController characterController;
+
+    private void Start () {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update () {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+}
