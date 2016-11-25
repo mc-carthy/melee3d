405fc92014c2278429947470beceb05d27a8f5df
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-25 13:43:03 +0000

    Create GameManager singleton

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	[SerializeField]
+	private GameObject player;
+	public GameObject Player {
+		get {
+			return player;
+		}
+	}
+
+	private bool isGameOver;
+	public bool IsGameOver {
+		get {
+			return isGameOver;
+		}
+	}
+
+	private void Awake () {
+		MakeSingleton();
+	}
+
+	public void PlayerHit (int currentHP) {
+		if (currentHP > 0) {
+			isGameOver = false;
+		} else {
+			isGameOver = true;
+		}
+	}
+
+	private void MakeSingleton() {
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+	}
+}
