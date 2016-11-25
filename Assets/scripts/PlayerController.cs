f0e9f3e497f68149deaeba0d10ae25e139df0c6f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-25 11:54:50 +0000

    Player turns to face mouse using floor plane and raycasting

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,7 +4,11 @@ public class PlayerController : MonoBehaviour {
 
 	[SerializeField]
     private float moveSpeed = 10f;
+    [SerializeField]
+    private LayerMask layerMask;
     private CharacterController characterController;
+    private Vector3 currentLookTarget = Vector3.zero;
+    private float turnSpeed = 10f;
 
     private void Start () {
         characterController = GetComponent<CharacterController>();
@@ -13,5 +17,21 @@ public class PlayerController : MonoBehaviour {
     private void Update () {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection.normalized * moveSpeed);
+    }
+
+    private void FixedUpdate () {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 500, Color.blue);
+
+        if (Physics.Raycast(ray, out hit, 500, layerMask, QueryTriggerInteraction.Ignore)) {
+            if (hit.point != currentLookTarget) {
+                currentLookTarget = hit.point;
+            }
+            Vector3 targetPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            Quaternion rotation = Quaternion.LookRotation(targetPos - transform.position);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.fixedDeltaTime * turnSpeed);
+        }
     }
 }
