156c76710c497ba0c6e046f4e01d0d32e38f4644
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-25 12:36:41 +0000

    Enemies now follow player

---
+++ Assets/scripts/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class EnemyMove : MonoBehaviour {
+
+	[SerializeField]
+	private Transform player;
+	private NavMeshAgent nav;
+	private Animator anim;
+
+	private void Awake () {
+		Assert.IsNotNull(player);
+	}
+
+	private void Start () {
+		anim = GetComponent<Animator>();
+		nav = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update () {
+		nav.SetDestination(player.position);
+	}
+}
