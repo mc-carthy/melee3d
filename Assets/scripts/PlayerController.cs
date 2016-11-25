2a96587bab570191ee5c5476de262d2080a56a70
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-25 12:01:46 +0000

    Walking animation now functioning

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,16 +7,24 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private LayerMask layerMask;
     private CharacterController characterController;
+    private Animator anim;
     private Vector3 currentLookTarget = Vector3.zero;
     private float turnSpeed = 10f;
 
     private void Start () {
         characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update () {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection.normalized * moveSpeed);
+
+        if (moveDirection == Vector3.zero) {
+            anim.SetBool("isWalking", false);
+        } else {
+            anim.SetBool("isWalking", true);
+        }
     }
 
     private void FixedUpdate () {
