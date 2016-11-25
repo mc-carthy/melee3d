7e5852583fe73f0c30d00e35079583a452ba67b7
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-25 17:01:05 +0000

    Player can now be hurt and die

---
+++ Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	[SerializeField]
+	private int startingHealth = 100;
+	[SerializeField]
+	private float timeSinceLastHit = 2;
+
+	private CharacterController characterController;
+	private Animator anim;
+
+	private float timer;
+	private int currentHealth;
+
+	private void Start () {
+		anim = GetComponent<Animator>();
+		characterController = GetComponent<CharacterController>();
+		currentHealth = startingHealth;
+	}
+
+	private void Update () {
+		timer += Time.deltaTime;
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		if (timer >= timeSinceLastHit && !GameManager.Instance.IsGameOver) {
+			if (other.CompareTag("weapon")) {
+				TakeHit();
+				timer = 0;
+			}
+		}
+	}
+
+	private void TakeHit () {
+		if (currentHealth > 0) {
+			GameManager.Instance.PlayerHit(currentHealth);
+			anim.Play("hurt");
+			currentHealth -= 10; // TODO - Remove this hardcoded value
+		} else {
+			KillPlayer();
+		}
+	}
+
+	private void KillPlayer () {
+		anim.Play("die");
+		GameManager.Instance.PlayerHit(currentHealth);
+		characterController.enabled = false;
+	}
+
+}
