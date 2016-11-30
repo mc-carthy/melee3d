3cabb31a303b542fd202d87c76957a63cc961f4a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-30 15:13:01 +0000

    Add animation and functional buttons to main menu

---
+++ Assets/scripts/AnimationEvents.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour {
+
+	public void EnemyBeginAttack () {
+
+	}
+
+	public void EnemyEndAttack () {
+		
+	}
+
+	public void FireArrow () {
+
+	}
+
+	public void PlayerBeginAttack () {
+
+	}
+
+	public void PlayerEndAttack () {
+
+	}
+}
---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject hero;
+	[SerializeField]
+	private GameObject tanker;
+	[SerializeField]
+	private GameObject soldier;
+	[SerializeField]
+	private GameObject ranger;
+
+	private Animator heroAnim;
+	private Animator tankerAnim;
+	private Animator soldierAnim;
+	private Animator rangerAnim;
+
+	private void Start () {
+		heroAnim = hero.GetComponent<Animator>();
+		tankerAnim = tanker.GetComponent<Animator>();
+		soldierAnim = soldier.GetComponent<Animator>();
+		rangerAnim = ranger.GetComponent<Animator>();
+
+		StartCoroutine(Showcase());
+	}
+
+	public void Battle () {
+		SceneManager.LoadScene("main");
+	}
+
+	public void Quit () {
+		Application.Quit();
+	}
+
+	private IEnumerator Showcase () {
+		yield return new WaitForSeconds(1f);
+		heroAnim.Play("spinAttack");
+		yield return new WaitForSeconds(1f);
+		tankerAnim.Play("attack");
+		yield return new WaitForSeconds(1f);
+		soldierAnim.Play("attack");
+		yield return new WaitForSeconds(1f);
+		rangerAnim.Play("attack");
+		StartCoroutine(Showcase());
+	}
+
+}
